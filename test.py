c3f33b91a661cc23f49f3044b54b8050f0aa9f97
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-10-05 14:43:52 -0700

    Update test.py

--- test.py
+++ test.py
@@ -12,6 +12,19 @@ import  streamlit_tree_select
 import copy
 import streamlit.components.v1 as components
 from calendar import monthrange
+import pandas as pd  # pip install pandas openpyxl
+import plotly.express as px  # pip install plotly-express
+import streamlit as st  # pip install streamlit
+import streamlit_authenticator as stauth  # pip install streamlit-authenticator
+conn = st.experimental_connection('mysql', type='sql')
+# Perform query.
+df = conn.query('SELECT * from users;', ttl=600)
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.user_name} has a :{row.password}:")
+
+
+
 s3 = boto3.client('s3')
 
 #---------------------------define parameters--------------------------
