0247a04e8cba3a6bcba33e0461c433d83ff1313a
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-10-09 15:42:02 -0700

    Update test.py

--- test.py
+++ test.py
@@ -979,7 +979,7 @@ elif st.session_state["authentication_status"]:
             ChangeWidgetFontSize('Manage Account Mapping', '25px')
             col1,col2=st.columns(2)
             with col1:
-	        new_tenant_account=st.text_input("Enter new tenant account and press enter to apply:")
+                new_tenant_account=st.text_input("Enter new tenant account and press enter to apply:")
 	        if new_tenant_account:
 	            st.markdown("## Map **'{}'** to Sabra account".format(new_tenant_account)) 
 	            Sabra_main_account_value,Sabra_second_account_value=Manage_Account_Mapping(new_tenant_account)
