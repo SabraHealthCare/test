51bb8c49ff1e693cc64b0ce26144d1d78fa59d21
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-10-14 20:38:57 -0700

    Update authenticate.py

--- authenticate.py
+++ authenticate.py
@@ -186,17 +186,16 @@ class Authenticate:
             self._check_cookie()
 
             if not st.session_state['authentication_status']:
-                col1,col2=st.columns(2)
-                with col1:
-                    if location == 'main':
-                        login_form = st.form('Login')
-                    elif location == 'sidebar':
-                        login_form = st.sidebar.form('Login')
+                
+                if location == 'main':
+                    login_form = st.form('Login')
+                elif location == 'sidebar':
+                    login_form = st.sidebar.form('Login')
 
-                    login_form.subheader(form_name)
-                    self.username = login_form.text_input('Username').lower()
-                    st.session_state['username'] = self.username
-                    self.password = login_form.text_input('Password', type='password')
+                login_form.subheader(form_name)
+                self.username = login_form.text_input('Username').lower()
+                st.session_state['username'] = self.username
+                self.password = login_form.text_input('Password', type='password')
                 if login_form.form_submit_button('Login'):
                     if len(self.password)>0 and len(self.username)>0:
                         self._check_credentials()
