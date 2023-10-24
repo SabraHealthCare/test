abda508a7ed314e20a3af26f16c2f4e564e0041e
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-10-24 13:49:10 -0700

    Update test.py

--- test.py
+++ test.py
@@ -412,7 +412,7 @@ def Save_File_toS3(uploaded_file, bucket, key):
 # For updating account_mapping, entity_mapping, latest_month_data, only for operator use
 def Update_File_inS3(bucket,key,new_data,operator,month=None,how = "replace"):  # how = replace, append...
     original_file =s3.get_object(Bucket=bucket, Key=key)
-    if if original_file: 
+    if original_file: 
         original_data=pd.read_csv(BytesIO(original_file['Body'].read()),header=0)
         st.write(original_data)
     else:
