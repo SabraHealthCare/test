b529932a3f487d408331704c58631ffd559a45c7
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-10-28 04:38:40 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1161,7 +1161,7 @@ elif st.session_state["authentication_status"] and st.session_state["operator"]=
                 # upload summary
                 upload_summary=data[["Operator","TIME","Latest_Upload_Time"]].drop_duplicates(["Operator","TIME","Latest_Upload_Time"]) 
                 upload_summary["TIME"]=upload_summary["TIME"].apply(lambda x: "{}.{}".format(str(x)[0:4],month_abbr[int(str(x)[4:6])]))
-                col1,col2,col3=st.columns((1,1,2))
+                col1,col2,col3=st.columns((3,1,1))
                 with col2:
                     upload_summary=filters_widgets(upload_summary,["Operator","TIME"])
                 with col1:
