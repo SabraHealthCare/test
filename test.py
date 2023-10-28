71a41d2beb3172042acf2a03c085431c7d73fc81
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-10-28 14:54:26 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1200,9 +1200,9 @@ elif st.session_state["authentication_status"] and st.session_state["operator"]=
                 no_button=st.button("No,Just download. I won't upload data this time.", on_click=clicked, args=["no_button"])          
                 if yes_button:
                     data["EPM_Formula"]="Uploaded"
-		    Save_CSV_ToS3(data,bucket_PL,monthly_reporting_path)
                     st.download_button(label="Download",data=download_file,file_name="Operator reporting data.csv",mime="text/csv"):
-                 
+                    st.download_button(label="Just download. I won't upload data this time.",data=download_file,file_name="Operator reporting data.csv",mime="text/csv"):
+                    
 		if no_button:
                     st.download_button(label="Just download. I won't upload data this time.",data=download_file,file_name="Operator reporting data.csv",mime="text/csv"):
                     
