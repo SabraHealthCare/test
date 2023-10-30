d02a29ee3142f04f0fd3365d13c452370a064680
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-10-30 10:21:40 -0700

    Update test.py

--- test.py
+++ test.py
@@ -994,11 +994,11 @@ if 'clicked' not in st.session_state:
 col1,col2=st.columns(2)
 with col1:
     authenticator.login('Login', bucket_PL,config,'main')
-    
+authenticator.logout('Logout', 'main')    
 if st.session_state["authentication_status"] is False:
     st.error('Username/password is incorrect')
 
-authenticator.logout('Logout', 'main')
+    
 #---------------operator account-----------------------
 elif st.session_state["authentication_status"]: #and st.session_state["operator"]!="sabra":
     operator=st.session_state["operator"]
