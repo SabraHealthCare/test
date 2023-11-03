1fad0ef1e72668f7d19ac765074f6f81553b861c
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-11-03 15:24:19 -0700

    Update test.py

--- test.py
+++ test.py
@@ -732,7 +732,8 @@ def View_Summary(uploaded_file):
     latest_month_data=latest_month_data.merge(BPC_Account, left_on="Sabra_Account", right_on="BPC_Account_Name",how="left")
     latest_month_data=latest_month_data.merge(entity_mapping[["Property_Name"]], on="ENTITY",how="left")
 
-    missing_check=latest_month_data[["Property_Name","Category","ENTITY",latest_month]][missing_check["Category"].isin(['Revenue','Patient Days','Operating Expenses',"Facility Information"])].groupby(["Property_Name","Category","ENTITY"]).sum().reset_index(drop=False)
+    missing_check=latest_month_data[["Property_Name","Category","ENTITY",latest_month]][latest_month_data["Category"].\
+	    isin(['Revenue','Patient Days','Operating Expenses',"Facility Information"])].groupby(["Property_Name","Category","ENTITY"]).sum().reset_index(drop=False)
     missing_check=missing_check[missing_check[latest_month]==0]
 
 	
