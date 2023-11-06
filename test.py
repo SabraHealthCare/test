7cfaeb22585819675ba507b14ad0f96c221a40f7
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-11-05 17:26:53 -0800

    Update test.py

--- test.py
+++ test.py
@@ -933,7 +933,7 @@ def PL_Process_Main(entity_i,sheet_type):
 	    # check the latest reporting month
             if latest_month=="2023":	    
                 latest_month=max_month_cols
-                col4,col5,col6=st.columns([4,1,6])
+                col4,col5,col6=st.columns([2,1,2])
                 with col4:
                     st.warning("The latest reporting month is: {}/{}. Is it true?".format(latest_month[4:6],latest_month[0:4])) 
                 with col5:		
