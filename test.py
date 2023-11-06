d5756f76c484760910c678e83c576ecedf666567
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-11-05 17:05:22 -0800

    Update test.py

--- test.py
+++ test.py
@@ -744,7 +744,7 @@ def View_Summary(uploaded_file):
 	
     if missing_check.shape[0]>0:
         st.error("No data detected for below matrix. ")
-        st.dataframe(missing_check[["Property_Name","Category",latest_month]].style.apply('background-color: {red}', subset=[latest_month]),
+        st.dataframe(missing_check[["Property_Name","Category",latest_month]].style.apply('background-color:red', subset=[latest_month]),
 		    column_config={
 			        "Property_Name": "Property",
 			        "Category":"Sabra account (total)"},
